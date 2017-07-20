f48537a160d60ae73e5d080ac5066e40979798de
SmartEggAnn <815177551@qq.com>
2017-07-20 22:11:19 +0800

    snp.cnv.data; reannotate.CNV.res

---
+++ reannotate.CNV.res/app.R
@@ -0,0 +1,47 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Licor
+
+library(shiny)
+library(saasCNV)
+
+data(seq.data)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+  # Application title
+  titlePanel("function: snp.cnv.data"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarPanel(
+    textInput(inputId = "only.CNV",
+              label = "only.CNV:",
+              value = TRUE)
+    
+  ),
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    tableOutput("res")
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  gene.anno <- read.delim(file="refGene_hg19.txt.gz", as.is=TRUE, comment.char="")
+  data(seq.cnv)
+  output$res <- renderTable({
+    reannotate.CNV.res(res=seq.cnv, gene=gene.anno, only.CNV=input$only.CNV)
+  })
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ snp.cnv.data/app.R
@@ -0,0 +1,49 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Licor
+
+library(shiny)
+library(saasCNV)
+
+data(seq.data)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+  # Application title
+  titlePanel("function: snp.cnv.data"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarPanel(
+    numericInput(inputId = "min.chr.probe",
+              label = "min.chr.probe:",
+              value = 100),
+    textInput(inputId = "verbose",
+              label = "verbose:",
+              value = TRUE)
+    
+  ),
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    tableOutput("res")
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  snp_table <- read.delim(file="snp_table.txt.gz", as.is=TRUE)
+  output$res <- renderTable({
+    head(snp.cnv.data(snp=snp_table, min.chr.probe=input$min.chr.probe, verbose=input$verbose))
+  })
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
+
